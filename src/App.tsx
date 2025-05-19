ee63a829167bf2bdb02abc092ec67c68cc594526
Gabriel BF <gbbolonhez@gmail.com>
2025-05-19 19:12:26 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Menu } from "./components/Menu/menu";
+
+function App() {
+  return (
+    <Menu></Menu>
+  );
+}
+
+export default App;
---
+++ src/components/Menu/menu.tsx
@@ -0,0 +1,10 @@
+const Menu = () => {
+    return (
+    <div className="App ">
+        <div>Menu</div>
+        <h1>Test</h1>
+    </div>
+    )
+}
+
+export { Menu };
