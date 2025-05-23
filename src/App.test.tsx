5f9fd9bdc9d9ef71dbc3c7d277a40f3888bba62c
Gabriel BF <gbbolonhez@gmail.com>
2025-05-23 18:49:58 -0300

    feat: E2E test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,49 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { gitApi } from './api/github';
+import {fireEvent} from '@testing-library/react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe ('App', () => {
+
+  let user = ""
+  it ("Must receive data on response, case the user existis", () => {
+    user = "Gabolonhez"
+    render (<App/>)
+
+    jest.spyOn(gitApi,"getUser")
+
+    const input = screen.getByRole("textbox", {name: "User"})
+    const button = screen.getByRole("button", {name: "Entrar"});
+
+    fireEvent.change(input, {
+      target: {value: user}
+    })
+    
+    fireEvent.click(button)
+
+    expect (gitApi.getUser).toHaveBeenCalled()
+     
+    const response = await gitApi.getUser(user)
+    expect(response).toHaveProperty("login")
+  })
+
+  it ("Must not receive data on response, case the user doesn't existis", () => {
+  user = "Gabolonhez-enwaewa"
+    render (<App/>)
+
+    jest.spyOn(gitApi,"getUser")
+
+    const input = screen.getByRole("textbox", {name: "User"})
+    const button = screen.getByRole("button", {name: "Entrar"});
+
+    fireEvent.change(input, {
+      target: {value: user}
+    })
+    
+    fireEvent.click(button)
+
+    const response = await gitApi.getUser(user)
+    expect(response).toBe(undefined)
+
+})
+})
