85c754f906d958012b53c42ea06e266b0d485b65
Gabriel BF <gbbolonhez@gmail.com>
2025-05-22 23:09:08 -0300

    feat: adding API

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,47 +2,65 @@ import React from "react";
 import { Layout } from "../../components/Layout/Layout";
 import { useNavigate } from 'react-router-dom';
 import { MouseEvent, useState } from "react";
+import { gitApi } from "../../api/github"; 
 
 const Home = () => {
 
-    const [user, setUser] = useState('')
+    const [user, setUser] = useState('');
+    const [invalid, setInvalid] = useState(false);
 
     const navigate = useNavigate();
 
     const handleClick = async (event: MouseEvent) => {
         event.preventDefault();
+        setInvalid(false); 
+
         if (user.length === 0) {
-            alert("Informe o usuário do Github")
-            return
+            alert("Informe o usuário do Github");
+            setInvalid(true); 
+            return;
         }
 
-        navigate(`/${user}`)
-    }
-
+        try {
+            const response = await gitApi.getUser(user); 
+            
+            navigate(`/${response.login}`); 
+            
+        } catch (error: any) { 
+            console.error(error); 
+            setInvalid(true); 
+            alert("Usuário não encontrado. Verifique o nome e tente novamente.");
+        }
+    }; 
 
     return (
         <Layout>
             <div className="col-4 m-auto">
                 <h1>Login</h1>
-                 <div className="mb-3">
-                     <label htmlFor="gitUser" className="form-label">Usuário do Github</label>
-                     <input 
-                     type="text" 
-                     aria-label="User" 
-                     className="form-control" 
-                     id="gitUser" 
-                     aria-describedby="userHelp" 
-                     placeholder="Informe o usuário do seu Github"
-                     value={user}
-                     onChange={event => setUser(event.target.value)}
-                     />
-                    <div id="userHelp" className="form-text">
-                    </div>
+                <div className="mb-3">
+                    <label htmlFor="gitUser" className="form-label">Usuário do Github</label>
+                    <input 
+                    type="text" 
+                    aria-label="User" 
+                    className="form-control" 
+                    id="gitUser" 
+                    aria-describedby="userHelp" 
+                    placeholder="Informe o usuário do seu Github"
+                    value={user}
+                    onChange={event => setUser(event.target.value)}
+                    />
+                    {
+                        invalid && ( 
+                            <div id="userHelp" className="form-text text-danger"> 
+                                Usuário inválido!
+                            </div>
+                        )
+                    }
                 </div>
                 <button onClick={handleClick} type="button" className="btn btn-primary">Entrar</button>
             </div>
         </Layout>
-    )
-} 
+    );
+}; 
 
 export { Home };
